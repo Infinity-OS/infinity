58d94d32667c13e20f901e2693d9ca84c46d4ebe
Gil Mendes <gil00mendes@gmail.com>
2017-02-18 18:43:21 +0000

    The kernel linking is working 😱

--- source/kernel/src/lib.rs
+++ source/kernel/src/lib.rs
@@ -1,3 +1,10 @@
+#![feature(lang_items)]
+#![no_std]
+
+#[no_mangle]
 fn start() -> ! {
     loop { }
 }
+
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
--- utilities/build/builders.py
+++ utilities/build/builders.py
@@ -7,7 +7,7 @@ ld_script_builder = Builder(action = Action(
     '$GENCOMSTR'))
 
 # Builder to run the "cargo rustc" command
-cargo_builder = Builder(action = Action('cargo rustc --manifest-path $SOURCE.abspath $CARGOFLAGS --lib -- --emit=obj=$TARGET', '$CARGOCOMSTR'),
+cargo_builder = Builder(action = Action('cargo rustc --manifest-path $SOURCE.abspath $CARGOFLAGS --lib --target x86_64-unknown-linux-gnu -- --emit=obj=$TARGET', '$CARGOCOMSTR'),
                         source_factory = SCons.Node.FS.Entry,
                         source_scanner = SCons.Defaults.DirScanner,
                         multi = 1)
