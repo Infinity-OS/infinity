aa02b247ddc2f45abbf89048b45d78017d1ddf8c
Gil Mendes <gil00mendes@gmail.com>
2017-02-25 17:22:41 +0000

    Show file and line on the panic function

--- source/kernel/src/lib.rs
+++ source/kernel/src/lib.rs
@@ -88,6 +88,10 @@ extern "C" fn eh_personality() {}
 
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub extern "C" fn panic_fmt() -> ! {
-    loop {}
+pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str,
+                        line: u32) -> !
+{
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop{}
 }
