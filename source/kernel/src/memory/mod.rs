516d32c4ff6ad021adb7e8262323442b4c149f31
Gil Mendes <gil00mendes@gmail.com>
2017-02-28 12:22:08 +0000

    issue #7 - Commit missing changes for the paging system

--- source/kernel/src/memory/mod.rs
+++ source/kernel/src/memory/mod.rs
@@ -1,21 +1,29 @@
 pub use self::area_frame_allocator::AreaFrameAllocator;
+pub use self::paging::test_paging;
+use self::paging::PhysicalAddress;
 
+mod paging;
 mod area_frame_allocator;
 
+/// Size of a page
 pub const PAGE_SIZE: usize = 4096;
 
-
+/// Describes a memory frame
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Frame {
     number: usize,
 }
 
 impl Frame {
-    //corresponding frame for a physical address
+    /// Corresponding frame for a physical address
     fn containing_address(address: usize) -> Frame {
         Frame{ number: address / PAGE_SIZE }
     }
 
+    /// Get the frame's starting address
+    fn start_address(&self) -> PhysicalAddress {
+        self.number * PAGE_SIZE
+    }
 }
 
 pub trait FrameAllocator {
