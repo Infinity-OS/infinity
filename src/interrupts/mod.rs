4e40871a1362c2cb8e408a57bbf8ac0116d4eb5c
Gil Mendes <gil00mendes@gmail.com>
2017-03-18 16:53:34 +0000

    Add a handler to catch page faults

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -21,6 +21,26 @@ macro_rules! handler {
     }}
 }
 
+/// This macro creates a wrapper for the handler  to exception that support error codes, in order to
+/// get the exception stack frame to pass it to the actual exception handler, and the error code.
+macro_rules! handler_with_error_code {
+    ($name: ident) => {{
+        #[naked]
+        extern "C" fn wrapper() -> ! {
+            unsafe {
+                asm!("pop rsi // pop error code into rsi
+                    mov rdi, rsp
+                    sub rsp, 8 // align the stack pointer
+                    call $0"
+                    :: "i"($name as extern "C" fn(&ExceptionStackFrame, u64) -> !)
+                    : "rdi" : "intel");
+                ::core::intrinsics::unreachable();
+            }
+        }
+        wrapper
+    }}
+}
+
 // The IDT is allocated statically to ensure that this stays in memory until the end of the kernel
 // execution.
 lazy_static! {
@@ -32,9 +52,20 @@ lazy_static! {
         // set the handler for the zero division exception
         idt.set_handler(0, handler!(divide_by_zero_handler));
         idt.set_handler(6, handler!(invalid_opcode_handler));
+        idt.set_handler(14, handler_with_error_code!(page_fault_exception));
 
         idt
     };
+}
+
+bitflags! {
+    flags PageFaultErrorCode: u64 {
+        const PROTECTION_VIOLATION = 1 << 0,
+        const CAUSED_BY_WRITE = 1 << 1,
+        const USER_MODE = 1 << 2,
+        const MALFORMED_TABLE = 1 << 3,
+        const INSTRUCTION_FETCH = 1 << 4,
+    }
 }
 
 #[derive(Debug)]
@@ -64,5 +95,16 @@ extern "C" fn divide_by_zero_handler(stack_frame: &ExceptionStackFrame) -> ! {
 extern "C" fn invalid_opcode_handler(stack_frame: &ExceptionStackFrame) -> ! {
     println!("\nEXCEPTION: INVALID OPCODE at {:#x}\n{:#?}\n",
         stack_frame.instruction_pointer, stack_frame);
+    loop {}
+}
+
+extern "C" fn page_fault_exception(stack_frame: &ExceptionStackFrame, error_code: u64) -> ! {
+    use x86::shared::control_regs;
+
+    println!("\nEXCEPTION: PAGE FAULT while accessing {:#x}\
+        \nerror code: {:?}\n{:#?}",
+        unsafe { control_regs::cr2() },
+        PageFaultErrorCode::from_bits(error_code).unwrap(),
+        stack_frame);
     loop {}
 }
