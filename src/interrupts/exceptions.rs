36111a59aed7846654d2d3cd80129eae0ce12dd4
Gil Mendes <gil00mendes@gmail.com>
2017-03-25 14:29:49 +0000

    #15 - implement all exception handlers

--- src/interrupts/exceptions.rs
+++ src/interrupts/exceptions.rs
@@ -1,44 +1,138 @@
-use x86_64::structures::idt::{Idt, ExceptionStackFrame, PageFaultErrorCode};
+use x86_64::structures::idt::{ExceptionStackFrame, PageFaultErrorCode};
 
 /// Handler for the division by zero exception
-pub extern "x86-interrupt" fn divide_by_zero_handler(stack_frame: &mut ExceptionStackFrame) {
+pub extern "x86-interrupt" fn divide_by_zero(stack_frame: &mut ExceptionStackFrame) {
     // print out the error message and the stack_frame
-    println!("\nEXCEPTION: DIVIDE BY ZERO\n{:#?}", stack_frame);
+    println!("\nDivide by zero fault at {:>02x}:{:>016x}", stack_frame.code_segment, stack_frame.instruction_pointer);
     loop {}
 }
 
 /// Handler for a debug exception
-pub extern "x86-interrupt" fn debug_handler(stack_frame: &mut ExceptionStackFrame) {
-    println!("\nEXCEPTION: DEBUG\n{:#?}", stack_frame);
+pub extern "x86-interrupt" fn debug(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nDebug trap at {:>02x}:{:>016x}", stack_frame.code_segment, stack_frame.instruction_pointer);
     loop {}
 }
 
-/// Handler for a invalid opcode exception
-pub extern "x86-interrupt" fn invalid_opcode_handler(stack_frame: &mut ExceptionStackFrame) {
-    println!("\nEXCEPTION: INVALID OPCODE at {:#x}\n{:#?}\n",
-             stack_frame.instruction_pointer, stack_frame);
-    loop {}
-}
-
-/// Handler for page faults
-pub extern "x86-interrupt" fn page_fault_exception(stack_frame: &mut ExceptionStackFrame, error_code: PageFaultErrorCode) {
-    use x86_64::registers::control_regs;
-
-    println!("\nEXCEPTION: PAGE FAULT while accessing {:#x}\nerror code: {:?}\n{:#?}",
-             control_regs::cr2(),
-             error_code,
-             stack_frame);
+/// Handler for a non-maskable interrupt exception
+pub extern "x86-interrupt" fn non_maskable(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nNon-maskable interrupt at {:>02x}:{:>016x}", stack_frame.code_segment, stack_frame.instruction_pointer);
     loop {}
 }
 
 /// Handler to catch breakpoint exceptions
-pub extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
-    println!("\nEXCEPTION: BREAKPOINT at {:#x}\n{:#?}",
-             stack_frame.instruction_pointer, stack_frame);
+pub extern "x86-interrupt" fn breakpoint(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nBreakpoint trap at {:>02x}:{:>016x}", stack_frame.code_segment, stack_frame.instruction_pointer);
+}
+
+/// Handler for a overflow exception
+pub extern "x86-interrupt" fn overflow(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nOverflow trap at {:>02x}:{:>016x}", stack_frame.code_segment, stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a bound range exceeded exception
+pub extern "x86-interrupt" fn bound_range_exceeded(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nBound range exceeded fault at {:>02x}:{:>016x}", stack_frame.code_segment, stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a invalid opcode exception
+pub extern "x86-interrupt" fn invalid_opcode(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nInvalid opcode fault at {:>02x}:{:>016x}", stack_frame.code_segment, stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a device not found exception
+pub extern "x86-interrupt" fn device_not_available(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nDevice not available fault at {:>02x}:{:>016x}", stack_frame.code_segment, stack_frame.instruction_pointer);
+    loop {}
 }
 
 /// Handler to catch double faults
-pub extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
-    println!("\nEXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+pub extern "x86-interrupt" fn double_fault(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
+    println!("\nDouble fault: {:x} at {:>02x}:{:>016x}", error_code, stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a invalid tss exception
+pub extern "x86-interrupt" fn invalid_tss(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
+    println!("\nInvalid TSS fault: {:x} at {:>02x}:{:>016x}", error_code, stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a segment not present exception
+pub extern "x86-interrupt" fn segment_not_present(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
+    println!("\nSegment not present fault: {:x} at {:>02x}:{:>016x}", error_code, stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a stack segment fault
+pub extern "x86-interrupt" fn stack_segment_fault(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
+    println!("\nStack segment fault: {:x} at {:>02x}:{:>016x}", error_code, stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a general protection fault
+pub extern "x86-interrupt" fn general_protection_fault(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
+    println!("\nProtection fault: {:x} at {:>02x}:{:>016x}", error_code, stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for page faults
+pub extern "x86-interrupt" fn page_fault(stack_frame: &mut ExceptionStackFrame, error_code: PageFaultErrorCode) {
+    use x86_64::registers::control_regs;
+
+    println!("\nPage fault while accessing {:>015x}\nerror code: {:?} at {:>02x}:{:>016x}",
+             control_regs::cr2(),
+             error_code,
+             stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a FPU floating point fault
+pub extern "x86-interrupt" fn x87_floating_point(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nFPU floating point fault at {:>02x}:{:>016x}", stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for alignment check exception
+pub extern "x86-interrupt" fn alignment_check(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
+    println!("\nAlignment check fault: {:x} at {:>02x}:{:>016x}", error_code, stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a machine check exception
+pub extern "x86-interrupt" fn machine_check(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nMachine check fault at {:>02x}:{:>016x}", stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a SIMD floating point exception
+pub extern "x86-interrupt" fn simd_floating_point(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nSIMD floating point fault at {:>02x}:{:>016x}", stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a virtualization exception
+pub extern "x86-interrupt" fn virtualization(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nVirtualization fault at {:>02x}:{:>016x}", stack_frame.code_segment,
+             stack_frame.instruction_pointer);
+    loop {}
+}
+
+/// Handler for a security exception
+pub extern "x86-interrupt" fn security_exception(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
+    println!("\nSecurity exception: {:x} at {:>02x}:{:>016x}", error_code, stack_frame.code_segment,
+             stack_frame.instruction_pointer);
     loop {}
 }
--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -2,9 +2,8 @@
 
 use memory::MemoryController;
 use x86_64::structures::tss::TaskStateSegment;
-use x86_64::structures::idt::{Idt, ExceptionStackFrame, PageFaultErrorCode};
+use x86_64::structures::idt::Idt;
 use spin::Once;
-use self::exceptions::*;
 
 mod gdt;
 mod exceptions;
@@ -19,16 +18,32 @@ lazy_static! {
         let mut idt = Idt::new();
 
         // Set up exceptions
-        idt.divide_by_zero.set_handler_fn(divide_by_zero_handler);
-        idt.debug.set_handler_fn(debug_handler);
-        idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
-        idt.page_fault.set_handler_fn(page_fault_exception);
-
+        idt.divide_by_zero.set_handler_fn(exceptions::divide_by_zero);
+        idt.debug.set_handler_fn(exceptions::debug);
+        idt.non_maskable_interrupt.set_handler_fn(exceptions::non_maskable);
+        idt.overflow.set_handler_fn(exceptions::overflow);
+        idt.breakpoint.set_handler_fn(exceptions::breakpoint);
+        idt.bound_range_exceeded.set_handler_fn(exceptions::bound_range_exceeded);
+        idt.invalid_opcode.set_handler_fn(exceptions::invalid_opcode);
+        idt.device_not_available.set_handler_fn(exceptions::device_not_available);
         unsafe {
-            idt.double_fault.set_handler_fn(double_fault_handler)
+            idt.double_fault.set_handler_fn(exceptions::double_fault)
                 .set_stack_index(DOUBLE_FAULT_IST_INDEX as u16);
         }
+        // 9 the coprocessor_segment_overrun is a discontinued exception
+        idt.invalid_tss.set_handler_fn(exceptions::invalid_tss);
+        idt.segment_not_present.set_handler_fn(exceptions::segment_not_present);
+        idt.stack_segment_fault.set_handler_fn(exceptions::stack_segment_fault);
+        idt.general_protection_fault.set_handler_fn(exceptions::general_protection_fault);
+        idt.page_fault.set_handler_fn(exceptions::page_fault);
+        // 15 reserved
+        idt.x87_floating_point.set_handler_fn(exceptions::x87_floating_point);
+        idt.alignment_check.set_handler_fn(exceptions::alignment_check);
+        idt.machine_check.set_handler_fn(exceptions::machine_check);
+        idt.simd_floating_point.set_handler_fn(exceptions::simd_floating_point);
+        idt.virtualization.set_handler_fn(exceptions::virtualization);
+        // 21 through 29 reserved
+        idt.security_exception.set_handler_fn(exceptions::security_exception);
 
         idt
     };
